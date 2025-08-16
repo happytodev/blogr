065d00c3d7393f4ae2b687f339314f4ac2dcb1f4
Frédéric Blanc <frederic.blanc@itanea.fr>
2025-08-16 16:32:48 +0200

    feat(blogpost): expand fillable fields and add user relationship

--- src/Models/BlogPost.php
+++ src/Models/BlogPost.php
@@ -6,10 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class BlogPost extends Model
 {
-    protected $fillable = ['title', 'photo', 'content', 'slug'];
+    protected $fillable = [
+        'title',
+        'photo',
+        'content',
+        'slug',
+        'user_id',
+        'is_published',
+        'published_at',
+        'meta_title',
+        'meta_description',
+        'meta_keywords',
+        'tldr'
+    ];
 
     public function getTable()
     {
         return config('blogr.tables.prefix', '') . 'blog_posts';
+    }
+
+    // A blog post belongs to a user
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
 }
