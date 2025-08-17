eef2692e6d1900e888f76d4856382222355a3146
Frédéric Blanc <frederic.blanc@itanea.fr>
2025-08-18 01:53:15 +0200

    Allow the user to customize the color of the blog post list on the blog index page. Fixes #12

--- config/blogr.php
+++ config/blogr.php
@@ -2,10 +2,18 @@
 
 // config for Happytodev/Blogr
 return [
-    'posts_per_page' => 10,  // Nombre de posts par page
+    'posts_per_page' => 10,  // Number of posts per page
     'route' => [
         // Prefix for frontend routes, if empty, the blog will be the homepage
-        'prefix' => 'blog', 
+        'prefix' => 'blog',
         'middleware' => ['web'], // Middleware for frontend routes
     ],
+    'blog_index' => [
+        'cards' => [
+            'colors' => [
+                'background' => 'bg-green-50', // Background color of blog index cards
+                'top_border' => 'border-green-600', // Border color of blog index cards
+            ]
+        ]
+    ]
 ];
