30830f07ab1bb9cae9d5e3d314834a1f8eb9d74e
Frédéric Blanc <frederic.blanc@itanea.fr>
2025-08-19 17:58:16 +0200

    [Bug]: When a blog post has 'published' to false, I should not to be able to see it on the blog index page Fixes #22

--- src/Http/Controllers/BlogController.php
+++ src/Http/Controllers/BlogController.php
@@ -11,21 +11,27 @@ class BlogController
 {
     public function index()
     {
-        $posts = BlogPost::latest()->take(config('blogr.posts_per_page', 10))->get()->map(function ($post) {
-            if ($post->photo) {
-                $post->photo_url = Storage::temporaryUrl(
-                    $post->photo,
-                    now()->addHours(1) // URL valid for 1 hour
-                );
-            }
-            return $post;
-        });
+        $posts = BlogPost::latest()
+            ->where('is_published', true)
+            ->take(config('blogr.posts_per_page', 10))
+            ->get()
+            ->map(function ($post) {
+                if ($post->photo) {
+                    $post->photo_url = Storage::temporaryUrl(
+                        $post->photo,
+                        now()->addHours(1) // URL valid for 1 hour
+                    );
+                }
+                return $post;
+            });
         return View::make('blogr::blog.index', ['posts' => $posts]);
     }
 
     public function show($slug)
     {
-        $post = BlogPost::where('slug', $slug)->firstOrFail();
+        $post = BlogPost::where('slug', $slug)
+            ->where('is_published', true)
+            ->firstOrFail();
         $converter = new CommonMarkConverter();
         $post->content = $converter->convert($post->content);
         if ($post->photo) {
