0da7aaee392509460b1644df0efd7f0219ece9cb
Frédéric Blanc <frederic.blanc@itanea.fr>
2025-08-20 10:58:07 +0200

    Add Category and Tag models with relationships to BlogPost

--- src/Models/BlogPost.php
+++ src/Models/BlogPost.php
@@ -2,6 +2,8 @@
 
 namespace Happytodev\Blogr\Models;
 
+use Happytodev\Blogr\Models\Tag;
+use Happytodev\Blogr\Models\Category;
 use Illuminate\Database\Eloquent\Model;
 
 class BlogPost extends Model
@@ -17,7 +19,8 @@ class BlogPost extends Model
         'meta_title',
         'meta_description',
         'meta_keywords',
-        'tldr'
+        'tldr',
+        'category_id',
     ];
 
     public function getTable()
@@ -29,5 +32,18 @@ class BlogPost extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    // A blog post belongs to a category
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    // A blog post can have many tags
+    // Many-to-many relationship with Tag model
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class, config('blogr.tables.prefix', '') . 'blog_post_tag');
     }
 }
---
+++ src/Models/Category.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Happytodev\Blogr\Models;
+
+use Illuminate\Support\Str;
+use Happytodev\Blogr\Models\BlogPost;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    protected $fillable = ['name', 'slug', 'is_default'];
+
+    public function getTable()
+    {
+        return config('blogr.tables.prefix', '') . 'categories';
+    }
+
+    public function posts()
+    {
+        return $this->hasMany(BlogPost::class);
+    }
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($category) {
+            if (empty($category->slug)) {
+                $category->slug = Str::slug($category->name);
+            }
+        });
+    }
+}
