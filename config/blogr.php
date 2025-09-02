50cea211c0d647e232d9d8307980f2238792417b
Frédéric Blanc <frederic.blanc@itanea.fr>
2025-09-02 13:41:23 +0200

    feat: Add reading speed and reading time display configurations to blog settings

--- config/blogr.php
+++ config/blogr.php
@@ -18,5 +18,36 @@ return [
     ],
     'colors' => [
         'primary' => '#FA2C36'
-    ]
+    ],
+    /*
+    |--------------------------------------------------------------------------
+    | Reading Speed Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Configure the estimated reading speed for calculating post read time.
+    | Standard reading speeds:
+    | - Slow readers: 150-200 words per minute
+    | - Average readers: 200-250 words per minute
+    | - Fast readers: 250-300 words per minute
+    |
+    | The calculation includes the post title and content.
+    |
+    */
+    'reading_speed' => [
+        'words_per_minute' => 200, // Average reading speed
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Reading Time Display Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Configure how reading time is displayed on blog posts.
+    | You can enable/disable the display and customize the text format.
+    |
+    */
+    'reading_time' => [
+        'enabled' => true, // Enable/disable reading time display
+        'text_format' => 'Reading time: {time}', // Text format with {time} placeholder
+    ],
 ];
