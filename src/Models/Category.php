a9e9c5514c5b1f26af8c9603a25eb466d182abb0
Frédéric Blanc <frederic.blanc@itanea.fr>
2025-09-07 10:35:48 +0200

    feat: Add HasFactory trait to Category model for factory support

--- src/Models/Category.php
+++ src/Models/Category.php
@@ -4,10 +4,12 @@ namespace Happytodev\Blogr\Models;
 
 use Illuminate\Support\Str;
 use Happytodev\Blogr\Models\BlogPost;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
+    use HasFactory;
     protected $fillable = ['name', 'slug', 'is_default'];
 
     public function getTable()
