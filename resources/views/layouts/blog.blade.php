8b18ecf8899f5b4729923413008dc706847abf3a
Frédéric Blanc <frederic.blanc@itanea.fr>
2025-09-08 23:42:31 +0200

    feat: Enhance SEO meta tags with default values for improved search visibility

--- resources/views/layouts/blog.blade.php
+++ resources/views/layouts/blog.blade.php
@@ -8,31 +8,31 @@
     @yield('seo-data')
 
     <!-- Primary Meta Tags -->
-    <title>{{ $seoData['title'] ?? config('blogr.seo.default_title') }}</title>
-    <meta name="title" content="{{ $seoData['title'] ?? config('blogr.seo.default_title') }}">
-    <meta name="description" content="{{ $seoData['description'] ?? config('blogr.seo.default_description') }}">
-    <meta name="keywords" content="{{ $seoData['keywords'] ?? config('blogr.seo.default_keywords') }}">
+    <title>{{ $seoData['title'] ?? config('blogr.seo.default_title', 'Blog') }}</title>
+    <meta name="title" content="{{ $seoData['title'] ?? config('blogr.seo.default_title', 'Blog') }}">
+    <meta name="description" content="{{ $seoData['description'] ?? config('blogr.seo.default_description', 'Discover our latest articles and insights') }}">
+    <meta name="keywords" content="{{ $seoData['keywords'] ?? config('blogr.seo.default_keywords', 'blog, articles, news, insights') }}">
 
     <!-- Canonical URL -->
     <link rel="canonical" href="{{ $seoData['canonical'] ?? url()->current() }}">
 
     <!-- Open Graph / Facebook -->
-    <meta property="og:type" content="{{ $seoData['og_type'] ?? config('blogr.seo.og.type') }}">
+    <meta property="og:type" content="{{ $seoData['og_type'] ?? config('blogr.seo.og.type', 'website') }}">
     <meta property="og:url" content="{{ $seoData['canonical'] ?? url()->current() }}">
-    <meta property="og:title" content="{{ $seoData['title'] ?? config('blogr.seo.default_title') }}">
-    <meta property="og:description" content="{{ $seoData['description'] ?? config('blogr.seo.default_description') }}">
-    <meta property="og:image" content="{{ $seoData['image'] ?? asset(config('blogr.seo.og.image')) }}">
-    <meta property="og:image:width" content="{{ $seoData['image_width'] ?? config('blogr.seo.og.image_width') }}">
-    <meta property="og:image:height" content="{{ $seoData['image_height'] ?? config('blogr.seo.og.image_height') }}">
-    <meta property="og:site_name" content="{{ config('blogr.seo.site_name') }}">
+    <meta property="og:title" content="{{ $seoData['title'] ?? config('blogr.seo.default_title', 'Blog') }}">
+    <meta property="og:description" content="{{ $seoData['description'] ?? config('blogr.seo.default_description', 'Discover our latest articles and insights') }}">
+    <meta property="og:image" content="{{ $seoData['image'] ?? asset(config('blogr.seo.og.image', '/images/blogr.webp')) }}">
+    <meta property="og:image:width" content="{{ $seoData['image_width'] ?? config('blogr.seo.og.image_width', 1200) }}">
+    <meta property="og:image:height" content="{{ $seoData['image_height'] ?? config('blogr.seo.og.image_height', 630) }}">
+    <meta property="og:site_name" content="{{ config('blogr.seo.site_name', 'My Blog') }}">
 
     <!-- Twitter -->
     <meta property="twitter:card" content="summary_large_image">
     <meta property="twitter:url" content="{{ $seoData['canonical'] ?? url()->current() }}">
-    <meta property="twitter:title" content="{{ $seoData['title'] ?? config('blogr.seo.default_title') }}">
+    <meta property="twitter:title" content="{{ $seoData['title'] ?? config('blogr.seo.default_title', 'Blog') }}">
     <meta property="twitter:description"
-        content="{{ $seoData['description'] ?? config('blogr.seo.default_description') }}">
-    <meta property="twitter:image" content="{{ $seoData['image'] ?? asset(config('blogr.seo.og.image')) }}">
+        content="{{ $seoData['description'] ?? config('blogr.seo.default_description', 'Discover our latest articles and insights') }}">
+    <meta property="twitter:image" content="{{ $seoData['image'] ?? asset(config('blogr.seo.og.image', '/images/blogr.webp')) }}">
     @if (config('blogr.seo.twitter_handle'))
         <meta property="twitter:site" content="{{ config('blogr.seo.twitter_handle') }}">
         <meta property="twitter:creator" content="{{ config('blogr.seo.twitter_handle') }}">
@@ -40,7 +40,7 @@
 
     <!-- Additional Meta Tags -->
     <meta name="robots" content="{{ $seoData['robots'] ?? 'index, follow' }}">
-    <meta name="author" content="{{ $seoData['author'] ?? config('blogr.seo.site_name') }}">
+    <meta name="author" content="{{ $seoData['author'] ?? config('blogr.seo.site_name', 'My Blog') }}">
 
     @if (isset($seoData['published_time']))
         <meta property="article:published_time" content="{{ $seoData['published_time'] }}">
@@ -63,7 +63,7 @@
     @vite(['resources/css/app.css'])
 
     <!-- Structured Data (JSON-LD) -->
-    @if (config('blogr.seo.structured_data.enabled'))
+    @if (config('blogr.seo.structured_data.enabled', true))
         <script type="application/ld+json">
             {!! \Happytodev\Blogr\Helpers\SEOHelper::generateJsonLd($seoData) !!}
         </script>
