7f467d6c3cf7b5df528c384103c3042afa2479c3
Frédéric Blanc <frederic.blanc@itanea.fr>
2025-10-06 21:48:59 +0200

    feat: Add French and English translations for user labels in Blogr

---
+++ resources/lang/fr/blogr.php
@@ -0,0 +1,10 @@
+<?php
+
+// translations for Happytodev/Blogr
+return [
+    'users' => [
+        'navigation_label' => 'Utilisateurs',
+        'model_label' => 'Utilisateur',
+        'plural_model_label' => 'Utilisateurs',
+    ],
+];
