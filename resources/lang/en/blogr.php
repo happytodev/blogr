9dba30977d46066549e98cb221a17f71989555d2
Frédéric Blanc <frederic.blanc@itanea.fr>
2025-10-08 00:41:34 +0200

    feat: Add English translations for blog UI and series

--- resources/lang/en/blogr.php
+++ resources/lang/en/blogr.php
@@ -7,4 +7,45 @@ return [
         'model_label' => 'User',
         'plural_model_label' => 'Users',
     ],
+    
+    // Blog UI
+    'ui' => [
+        'table_of_contents' => 'Table of contents',
+        'back_to_all_posts' => 'Back to all posts',
+        'back_to_blog' => 'Back to blog',
+        'read_more' => 'Read more',
+        'read_post' => 'Read post',
+        'view_all_series' => 'View all series',
+        'no_posts_yet' => 'No posts yet',
+        'check_back_soon' => 'Check back soon for new content!',
+        'featured' => 'Featured',
+        'part_of_series' => 'Part of Series',
+        'posts_in_series' => ':count posts in this series',
+        'post_in_series' => ':count post in this series',
+        'current' => 'current',
+        'tags' => 'Tags',
+        'reading_time' => ':time min read',
+        'min_read' => ':minutes min read',
+        'untitled' => 'Untitled',
+    ],
+    
+    // Series
+    'series' => [
+        'title' => 'Blog Series',
+        'description' => 'Browse all our blog series and learn step by step.',
+        'no_series' => 'No series published yet',
+        'part_of_series' => 'Part of Series',
+        'posts_count' => ':count posts',
+        'view_series' => 'View series',
+        'started_on' => 'Started :date',
+        'all_posts_in_series' => 'All Posts in This Series',
+        'part_number' => 'Part :number',
+    ],
+    
+    // Dates
+    'date' => [
+        'published_on' => 'Published on :date',
+        'updated_on' => 'Updated on :date',
+        'draft' => 'Draft',
+    ],
 ];
