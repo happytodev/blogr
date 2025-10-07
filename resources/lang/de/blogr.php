8272d8cac6bc4d993f7f117359335245acee1b5e
Frédéric Blanc <frederic.blanc@itanea.fr>
2025-10-08 00:40:49 +0200

    feat: Add German translations for blog UI and series

---
+++ resources/lang/de/blogr.php
@@ -0,0 +1,51 @@
+<?php
+
+// translations for Happytodev/Blogr
+return [
+    'users' => [
+        'navigation_label' => 'Benutzer',
+        'model_label' => 'Benutzer',
+        'plural_model_label' => 'Benutzer',
+    ],
+    
+    // Blog UI
+    'ui' => [
+        'table_of_contents' => 'Inhaltsverzeichnis',
+        'back_to_all_posts' => 'Zurück zu allen Beiträgen',
+        'back_to_blog' => 'Zurück zum Blog',
+        'read_more' => 'Mehr lesen',
+        'read_post' => 'Beitrag lesen',
+        'view_all_series' => 'Alle Serien ansehen',
+        'no_posts_yet' => 'Noch keine Beiträge',
+        'check_back_soon' => 'Schauen Sie bald wieder vorbei für neue Inhalte!',
+        'featured' => 'Hervorgehoben',
+        'part_of_series' => 'Teil der Serie',
+        'posts_in_series' => ':count Beiträge in dieser Serie',
+        'post_in_series' => ':count Beitrag in dieser Serie',
+        'current' => 'aktuell',
+        'tags' => 'Tags',
+        'reading_time' => ':time Min. Lesezeit',
+        'min_read' => ':minutes Min. Lesezeit',
+        'untitled' => 'Ohne Titel',
+    ],
+    
+    // Series
+    'series' => [
+        'title' => 'Blog-Serien',
+        'description' => 'Durchsuchen Sie alle unsere Blog-Serien und lernen Sie Schritt für Schritt.',
+        'no_series' => 'Noch keine Serien veröffentlicht',
+        'part_of_series' => 'Teil der Serie',
+        'posts_count' => ':count Beiträge',
+        'view_series' => 'Serie anzeigen',
+        'started_on' => 'Begonnen am :date',
+        'all_posts_in_series' => 'Alle Beiträge in dieser Serie',
+        'part_number' => 'Teil :number',
+    ],
+    
+    // Dates
+    'date' => [
+        'published_on' => 'Veröffentlicht am :date',
+        'updated_on' => 'Aktualisiert am :date',
+        'draft' => 'Entwurf',
+    ],
+];
