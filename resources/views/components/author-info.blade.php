437426c36b95ae83e8ff4bb30bd9c805b07c979d
Frédéric Blanc <frederic.blanc@itanea.fr>
2025-10-15 17:31:27 +0200

    feat: update author info component to support profile links and improve avatar handling

--- resources/views/components/author-info.blade.php
+++ resources/views/components/author-info.blade.php
@@ -5,6 +5,18 @@
     $showAvatar = $showAvatar ?? config('blogr.display.show_author_avatar', true);
     $showPseudo = $showPseudo ?? config('blogr.display.show_author_pseudo', true);
     $displayName = $showPseudo && $author->slug ? $author->slug : $author->name;
+    $authorProfileEnabled = config('blogr.author_profile.enabled', true) && isset($author->slug) && !empty($author->slug);
+    $localesEnabled = config('blogr.locales.enabled', false);
+    
+    // Build route parameters if author profile is enabled
+    if ($authorProfileEnabled) {
+        $currentLocale = app()->getLocale();
+        if ($localesEnabled) {
+            $routeParams = ['locale' => $currentLocale, 'userSlug' => $author->slug];
+        } else {
+            $routeParams = ['userSlug' => $author->slug];
+        }
+    }
     
     // Size classes
     $sizeClasses = [
@@ -16,19 +28,37 @@
     $avatarClass = $sizeClasses[$size] ?? $sizeClasses['sm'];
 @endphp
 
-<div {{ $attributes->merge(['class' => 'flex items-center gap-2']) }}>
-    @if($showAvatar && $author->avatar)
-        <img 
-            src="{{ Storage::disk('public')->url($author->avatar) }}" 
-            alt="{{ $displayName }}"
-            class="{{ $avatarClass }} rounded-full object-cover"
-        />
-    @elseif($showAvatar)
-        <div class="{{ $avatarClass }} rounded-full bg-gray-300 dark:bg-gray-600 flex items-center justify-center font-semibold text-gray-700 dark:text-gray-200">
-            {{ strtoupper(substr($displayName, 0, 1)) }}
-        </div>
-    @endif
-    
-    <span class="text-gray-700 dark:text-gray-300">{{ $displayName }}</span>
-</div>
+@if($authorProfileEnabled)
+    <a href="{{ route('blog.author', $routeParams) }}" {{ $attributes->merge(['class' => 'flex items-center gap-2 hover:opacity-80 transition-opacity']) }}>
+        @if($showAvatar && $author->avatar)
+            <img 
+                src="{{ Storage::disk('public')->url($author->avatar) }}" 
+                alt="{{ $displayName }}"
+                class="{{ $avatarClass }} rounded-full object-cover"
+            />
+        @elseif($showAvatar)
+            <div class="{{ $avatarClass }} rounded-full bg-gray-300 dark:bg-gray-600 flex items-center justify-center font-semibold text-gray-700 dark:text-gray-200">
+                {{ strtoupper(substr($displayName, 0, 1)) }}
+            </div>
+        @endif
+        
+        <span class="text-gray-700 dark:text-gray-300">{{ $displayName }}</span>
+    </a>
+@else
+    <div {{ $attributes->merge(['class' => 'flex items-center gap-2']) }}>
+        @if($showAvatar && $author->avatar)
+            <img 
+                src="{{ Storage::disk('public')->url($author->avatar) }}" 
+                alt="{{ $displayName }}"
+                class="{{ $avatarClass }} rounded-full object-cover"
+            />
+        @elseif($showAvatar)
+            <div class="{{ $avatarClass }} rounded-full bg-gray-300 dark:bg-gray-600 flex items-center justify-center font-semibold text-gray-700 dark:text-gray-200">
+                {{ strtoupper(substr($displayName, 0, 1)) }}
+            </div>
+        @endif
+        
+        <span class="text-gray-700 dark:text-gray-300">{{ $displayName }}</span>
+    </div>
+@endif
 @endif
