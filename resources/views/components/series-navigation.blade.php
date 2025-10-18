f1d8fca5f338c4460e7290e39c3a53c211d7b9f2
Frédéric Blanc <frederic.blanc@itanea.fr>
2025-10-18 21:46:50 +0200

    feat: update series navigation to use translated slug for series link

--- resources/views/components/series-navigation.blade.php
+++ resources/views/components/series-navigation.blade.php
@@ -83,7 +83,7 @@
     </div>
     
     <div class="mt-4 pt-4 border-t border-blue-200">
-        <a href="{{ route('blog.series', ['locale' => $currentLocale, 'seriesSlug' => $series->slug]) }}" 
+        <a href="{{ route('blog.series', ['locale' => $currentLocale, 'seriesSlug' => $series->getTranslatedSlug($currentLocale)]) }}" 
            class="inline-flex items-center text-sm font-medium text-blue-600 hover:text-blue-800">
             <svg class="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"/>
