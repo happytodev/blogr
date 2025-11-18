42911a647bf15dfca9a8d0cfca1cdfa29953be6d
Frédéric Blanc <frederic.blanc@itanea.fr>
2025-11-18 20:13:30 +0100

    refactor: enhance blocks renderer to support adjacent block detection for color calculations

--- resources/views/components/blocks-renderer.blade.php
+++ resources/views/components/blocks-renderer.blade.php
@@ -5,14 +5,28 @@ $blocksData = is_string($blocks) ? json_decode($blocks, true) : $blocks;
 $blocksData = $blocksData ?? [];
 @endphp
 
-@foreach($blocksData as $block)
+@foreach($blocksData as $index => $block)
     @php
     $type = $block['type'] ?? null;
     $data = $block['data'] ?? [];
     $componentView = "blogr::components.blocks.{$type}";
+    $isTransitionBlock = str_starts_with($type, 'transition-');
+    
+    // Detect adjacent blocks for intelligent color calculations
+    $previousBlock = ($index > 0) ? $blocksData[$index - 1] : null;
+    $nextBlock = ($index < count($blocksData) - 1) ? $blocksData[$index + 1] : null;
     @endphp
     
     @if($type && view()->exists($componentView))
-        @include($componentView, ['data' => $data])
+        @if($isTransitionBlock)
+            {{-- Transition blocks positioned smoothly between blocks without overlap --}}
+            @include($componentView, [
+                'data' => $data,
+                'previousBlock' => $previousBlock,
+                'nextBlock' => $nextBlock
+            ])
+        @else
+            @include($componentView, ['data' => $data])
+        @endif
     @endif
 @endforeach
