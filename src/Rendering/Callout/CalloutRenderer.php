0fa40eb2236040114ede9eb54520b71a48b91d80
Frédéric Blanc <frederic.blanc@itanea.fr>
2026-07-07 00:09:06 +0200

    feat(callouts): always render icons, improve hierarchy, upgrade to Heroicons v2 32px
    - Icons now render with or without title (always visible)
    - Caution uses warning triangle, Danger uses prohibitive x-circle
    - All icons upgraded to Heroicons v2 outline at 32px with stroke-width 1.5
    - Title font-size 1.125rem, content font-size 0.75rem
    - hidden pseudo-elements on callout buttons

--- src/Rendering/Callout/CalloutRenderer.php
+++ src/Rendering/Callout/CalloutRenderer.php
@@ -20,16 +20,15 @@ class CalloutRenderer implements NodeRendererInterface
         $innerHtml = $childRenderer->renderNodes($node->children());
 
         $icon = match ($type) {
-            CalloutType::Tip => '<svg class="docs-callout__icon" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"/></svg>',
-            CalloutType::Info => '<svg class="docs-callout__icon" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/></svg>',
-            CalloutType::Danger => '<svg class="docs-callout__icon" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"/></svg>',
-            CalloutType::Caution => '<svg class="docs-callout__icon" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/></svg>',
+            CalloutType::Tip => '<svg class="docs-callout__icon" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><path d="M12 18v-5.25m0 0a6.01 6.01 0 0 0 1.5-.189m-1.5.189a6.01 6.01 0 0 1-1.5-.189m3.75 7.478a12.06 12.06 0 0 1-4.5 0m3.75 2.383a14.406 14.406 0 0 1-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 1 0-7.517 0c.85.493 1.509 1.333 1.509 2.316V18"/></svg>',
+            CalloutType::Info => '<svg class="docs-callout__icon" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><path d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z"/></svg>',
+            CalloutType::Caution => '<svg class="docs-callout__icon" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><path d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z"/></svg>',
+            CalloutType::Danger => '<svg class="docs-callout__icon" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><path d="M9 9l6 6m0-6l-6 6M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/></svg>',
         };
 
-        $titleHtml = '';
-        if ($title) {
-            $titleHtml = '<p class="docs-callout__title">'.$icon.' '.htmlspecialchars($title).'</p>';
-        }
+        $titleHtml = $title
+            ? '<p class="docs-callout__title">'.$icon.' '.htmlspecialchars($title).'</p>'
+            : '<p class="docs-callout__title docs-callout__title--icon-only">'.$icon.'</p>';
 
         $classes = 'docs-callout docs-callout--'.$type->value;
 
